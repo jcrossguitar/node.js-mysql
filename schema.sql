902e95ea70a967f8ef64d01dff4c75ca2450ecb7
jon <jcrossguitar@gmail.com>
2019-09-28 12:32:21 -0700

    I reworked the area in schema to have more of a correct format

--- schema.sql
+++ schema.sql
@@ -3,14 +3,10 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
-CREATE TABLE products;
-
-INSERT INTO products (name) VALUES ('Bilal');
-INSERT INTO products (name) VALUES ('Brianne');
-INSERT INTO products (name) VALUES ('Vincent');
-
-INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Everybody Loves Raymond', 'tv', 500, 1);
-INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Big Bang Theory', 'tv', 900, 1);
-INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Top Gun', 'movie', 200, 2);
-INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Whiskey', 'grown-up', 300, 2);
-INSERT INTO parties (party_name, party_type, party_cost, client_id) VALUES ('Cigar', 'grown-up', 250, 3);
+CREATE TABLE products (
+    item_id INT NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100), NOT NULL,
+    price INT NOT NULL, 
+    stock_quantity INT NOT NULL
+);
