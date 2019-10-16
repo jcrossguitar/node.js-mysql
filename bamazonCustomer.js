5f803089119ed09b4b92be5f95d0fb7c7b5f403f
jon <jcrossguitar@gmail.com>
2019-10-16 12:39:10 -0700

    I was working with csv files and decided to use the info and create just the one schema.sql file for everything. I changed the port I was working on and it would not work so I switched back to 3306 and it worked great. I wanted to show that there was a price involed as well. The stock quantity is going down and we can quit using the word exit. I did not create code to add up the purchases, but I would be interested in possibly finding how to do that so that the price area is more accurate and functional.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,41 +1,140 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "Mfatgg123!",
   database: "bamazonDB"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+  if (err) return err;
+  loadItems();
 });
 
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What catches your eye?",
-      choices: [
-        "ID: 1, Guitar: American Standard Stratocaster, Color: Black, Year: 2019, Price: 1000, Stock Level: 52",
-        "ID: 2, Guitar: American Standard Telecaster, Color: Sunburst, Year: 2019, Price: 1100, Stock Level: 42",
-        "ID: 3, Guitar: Vintage Telecaster, Color: Natural, Year: 1952, Price: 11100, Stock Level: 42",
-        "Other"
-      ]
-    })
-  //Select all customers and return the result object:
-  connection.query("SELECT * FROM Fender", function (err, result, guitars) {
+function loadItems() {
+  // Selects all of the data from the MySQL products table
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    console.log(result);
+
+    // Display table in the terminal
+    console.table(res);
+
+    // Then prompt the customer for products to be purchased
+    productsPrompt(res);
   });
-};
+}
+
+// function readProducts() {
+//   console.log("Selecting all products...\n");
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     // Log all results of the SELECT statement
+//     console.log(res);
+//     connection.end();
+//   });
+// }
+  //Select all items and return the result object:
+  // connection.query("SELECT * FROM products", function (err, result, item_id) {
+  //   if (err) return err;
+  //   console.log(result);
+  // });
+  // Prompt the customer for a product ID
+function productsPrompt(itemsInStock) {
+  // prompt with instructions
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "What is the ID of the item you would you like to purchase? [Quit with quit]",
+        validate: function(val) {
+          return !isNaN(val) || val.toLowerCase() === "quit";
+        }
+      }
+    ])
+    .then(function(val) {
+      // Check if the user wants to quit the program
+      exitListener(val.choice);
+      var userPick = parseInt(val.choice);
+      var product = availableStock(userPick, itemsInStock);
+
+      // If there is a product with the id the user chose, prompt the customer for a desired quantity
+      if (product) {
+        // Pass the chosen product to numberOfItems
+        numberOfItems(product);
+      }
+      else {
+        // Else not available
+        console.log("\nThat item is not available at the moment.");
+        loadItems();
+      }
+    });
+}
+
+// Prompt the customer for a product quantity
+function numberOfItems(product) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like? [Quit with quit]",
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === "quit";
+        }
+      }
+    ])
+    .then(function(val) {
+      // Check if the user wants to quit the program
+      exitListener(val.quantity);
+      var quantity = parseInt(val.quantity);
+
+      // If there isn't enough of the chosen product and quantity, let the user know and re-run loadItems
+      if (quantity > product.stock_quantity) {
+        console.log("\nNot enough stock! Try again later!");
+        loadItems();
+      }
+      else {
+        // Otherwise run makePurchase, give it the product information and desired quantity to purchase
+        makePurchase(product, quantity);
+      }
+    });
+}
+
+// Purchase the desired quantity of the desired item
+function makePurchase(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+    [quantity, product.item_id],
+    function(err, res) {
+      // Let the user know the purchase was successful, re-run loadItems
+      console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s! " + product.price + " each!");
+      loadItems();
+    }
+  );
+}
+
+// Check to see if the product the user chose exists in the itemsInStock
+function availableStock(userPick, itemsInStock) {
+  for (var i = 0; i < itemsInStock.length; i++) {
+    if (itemsInStock[i].item_id === userPick) {
+      // If a matching product is found, return the product
+      return itemsInStock[i];
+    }
+  }
+  // Otherwise return null
+  return null;
+}
+
+// Check to see if the user wants to quit the program
+function exitListener(choice) {
+  if (choice.toLowerCase() === "quit") {
+    // Log a message and exit the current node process
+    console.log("Thank you for your time!");
+    process.exit(0);
+  }
+}
--- schema.sql
+++ schema.sql
@@ -3,48 +3,29 @@ CREATE database bamazonDB;
 
 USE bamazonDB;
 
-CREATE TABLE Fender (
-  id INT NOT NULL,
-  guitars VARCHAR(100) NULL,
-  color VARCHAR(100) NULL,
-  year INT NOT NULL,
-  price DECIMAL(6) NOT NULL,
-  stockLevel INT(3) NULL,
-  PRIMARY KEY (id)
+CREATE TABLE products (
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT(3) NULL,
+  PRIMARY KEY (item_id)
 );
 
-CREATE TABLE Gibson (
-  id INT NOT NULL,
-  guitars VARCHAR(100) NULL,
-  color VARCHAR(100) NULL,
-  year INT(4) NOT NULL,
-  price DECIMAL(6) NOT NULL,
-  stockLevel INT(3) NULL,
-  PRIMARY KEY (id)
-);
+SELECT * FROM products;
 
-CREATE TABLE Accessories (
-  id INT NOT NULL,
-  item VARCHAR(100) NULL,
-  brand VARCHAR(100) NOT NULL,
-  price DECIMAL(6) NOT NULL,
-  stockLevel INT(3) NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE Ampliphiers (
-  id INT NOT NULL,
-  guitarAmps VARCHAR(100) NULL,
-  watts INT(4),
-  color VARCHAR(100) NULL,
-  year INT(4) NOT NULL,
-  price DECIMAL(6) NOT NULL,
-  weight DECIMAL(2,2) NOT NULL,
-  stockLevel INT(3) NULL,
-  PRIMARY KEY (id)
-);
-
-SELECT * FROM Fener;
-select * from Gibson;
-select * from Accessories;
-select * from Ampliphiers;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Marshall JC100", "amps", 800.99, 22),
+("Fender Twin Reverb", "amps" ,2000.99, 5),
+("Danelectro '59 Modified New Old Stock (NOS)", "instruments", 350.99, 25),
+("Fender American Standard Stratocaster", "instruments", 1000.99, 52),
+("Fender American Standard Telecaster", "instruments", 1100.99, 42),
+("Fender Vintage Telecaster", "instruments", 11100.99, 2),
+("Gibson Les Paul Custom", "instruments", 1999.99, 22),
+("Gibson SG", "instruments", 2100.99, 15),
+("Gibson Melody Maker", "instruments", 500.99,50),
+("Fender Jaguar", "instruments", 800.99, 18),
+("ErnieBall", "strings", 6.99, 200),
+("ClearTone Elixer", "strings", 11.99, 100),
+("Hohner Blues Harp", "instruments", 12.99, 200),
+("Fender Mandolin", "instruments", 350.99, 200);
